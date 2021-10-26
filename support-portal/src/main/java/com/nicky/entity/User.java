ec27e04890df75922db85714c2557f5aef231dba
nkRev <87381923+nkRev@users.noreply.github.com>
2021-10-26 14:30:28 -0400

    user entity completed

---
+++ support-portal/src/main/java/com/nicky/entity/User.java
@@ -0,0 +1,43 @@
+package com.nicky.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+    private String userId;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+    private String profileImageUrl;
+    private Date lastLoginDate;
+    private Date lastLoginDateDisplay;
+    private Date joinDate; // sign up date
+    private String[] roles; // user_roles, admin_roles each type has capabilities to do something.
+    private String[] authorities; // what users can do wether it is complete CRUD or some form of it
+    private boolean active; // whether account is actively used or not
+    private boolean isNotLocked; // whether account is locked or not
+
+}
